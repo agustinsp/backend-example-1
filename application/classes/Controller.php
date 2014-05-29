d5aab7208e018c30c0b8400e1ab14edbb1bbb9a8
Agustin Vijoditz <vijoditz.agustin@gmail.com>
2014-05-29 11:07:58 -0600

    Removed unused ORM.php; renamed model test cases directory; Comments added to custom Controller.php

--- application/classes/Controller.php
+++ application/classes/Controller.php
@@ -1,12 +1,24 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+/**
+ * Custom abstract Controller class to add functionality to the Core Kohana_Controller.
+ * 
+ * @author Agustin Vijoditz <vijo.agus@gmail.com>
+ * @on May 29th, 2014
+ * @package BlogDemoKohana
+ * @license http://www.gnu.org/copyleft/gpl.html    GPL v3
+ */
 abstract class Controller extends Kohana_Controller
 {
-    
+    /**
+     * Outputs sent array as a JSON string and sends "Content-Type:application/json" headers
+     * 
+     * @param array $data
+     * @output JSON response
+     */
     protected function output_json($data)
     {
         $this->response->headers('Content-Type','application/json');
         $this->response->body(json_encode($data));
     }
-    
-}
+  }
